1871433a54efb67889e0ce83a469308deb2500ca
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-09 21:40:13 +0300

    Create new model for testimonial.

---
+++ Client/HotelProject.WebUI/Areas/Admin/Models/Testimonial/TestimonialViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.WebUI.Areas.Admin.Models.Testimonial
+{
+    public class TestimonialViewModel
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+        public string Subject { get; set; }
+        public string Comment { get; set; }
+    }
+}
