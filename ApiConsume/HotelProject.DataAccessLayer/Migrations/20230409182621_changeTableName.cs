a45bb6ecfcfa711ea821d4f688133ebc2e2ec532
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-09 21:39:40 +0300

    Create new migrations.

---
+++ ApiConsume/HotelProject.DataAccessLayer/Migrations/20230409182621_changeTableName.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelProject.DataAccessLayer.Migrations
+{
+    public partial class changeTableName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tests",
+                table: "Tests");
+
+            migrationBuilder.RenameTable(
+                name: "Tests",
+                newName: "Testimonials");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Testimonials",
+                table: "Testimonials",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Testimonials",
+                table: "Testimonials");
+
+            migrationBuilder.RenameTable(
+                name: "Testimonials",
+                newName: "Tests");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tests",
+                table: "Tests",
+                column: "Id");
+        }
+    }
+}
