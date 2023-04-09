6e3160d16b8a8e0372f4285b9ddf9254e928c476
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-09 21:39:54 +0300

    Add new controller.

---
+++ Client/HotelProject.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,100 @@
+﻿using HotelProject.BusinessLayer.Concrete;
+using HotelProject.DataAccessLayer.Concrete;
+using HotelProject.DataAccessLayer.EntityFramework;
+using HotelProject.WebUI.Areas.Admin.Models.Testimonial;
+using HotelProject.WebUI.Areas.Admin.Models.Testimonial;
+using HotelProject.WebUI.Methods;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TestimonialController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TestimonialController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44322/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<TestimonialViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddTestimonial() => View();
+        [HttpPost]
+        public async Task<IActionResult> AddTestimonial(TestimonialViewModel testimonialViewModel)
+        {
+    
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(testimonialViewModel);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44322/api/Testimonial", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44322/api/Testimonial?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44322/api/Testimonial/id?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<TestimonialViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(TestimonialViewModel model)
+        {
+            TestimonialManager stafManager = new TestimonialManager(new EfTestimonialDal(new Context()));
+            var Testimonial = stafManager.GetById(model.id);
+
+           
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44322/api/Testimonial/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Testimonial");
+            }
+            return View();
+        }
+    }
+}
+
+
