41f79bd46b2d1faca2d2b01c5d7f20b231baca5b
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-11 21:25:15 +0300

    Add auto mapping and auto mapping injection .

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -47,6 +47,8 @@ namespace HotelProject.WebApi
             services.AddScoped<ITestimonialDal, EfTestimonialDal>();
             services.AddScoped<ITestimonialService, TestimonialManager>();
 
+            services.AddAutoMapper(typeof(Startup));
+
             services.AddCors(option =>
             {
                 option.AddPolicy("HotelApiCors", options =>
