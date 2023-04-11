0fae81bd0b26485c70b0cdfd7db7b2481e4ed7b5
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-11 21:24:26 +0300

    Add dto classes.

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomAddDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class RoomAddDto
+    {
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="Bu alan zorunludur.")]
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int BedCount { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int BathCount { get; set; }
+        public string CoverImageUrl { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Description { get; set; }
+    }
+}
---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomUpdateDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto
+{
+    public class RoomUpdateDto
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string RoomNumber { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int BedCount { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int BathCount { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string CoverImageUrl { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public int Price { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [StringLength(500,ErrorMessage ="En Fazla 500 karakterlik bir açıklama girebilirsiniz.")]
+        public string Description { get; set; }
+    }
+}
