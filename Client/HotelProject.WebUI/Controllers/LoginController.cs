93ac514193d41c74328290ad5bf6a12ff9dc7242
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-17 00:12:45 +0300

    Create new controllers.

---
+++ Client/HotelProject.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.LoginDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginUserDto loginUserDto)
+        {
+           if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var result = await _signInManager.PasswordSignInAsync(loginUserDto.userName, loginUserDto.password, false, false);
+            if(result.Succeeded) 
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Client/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateUserDto model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+            var appUser = new AppUser()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName
+            };
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
