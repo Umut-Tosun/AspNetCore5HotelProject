c8d13f3e39e3d1d297f64db8aebb1097a1d00c6e
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-17 00:09:21 +0300

    Add register dto.

---
+++ Client/HotelProject.WebUI/Dtos/RegisterDto/CreateUserDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateUserDto
+    {
+        [Required(ErrorMessage ="Bu alan zorunludur.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Bu alan zorunludur.")]
+        [Compare("Password",ErrorMessage ="Şifreler uyuşmuyor.")]
+        public string ConfirmPassword { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ Client/HotelProject.WebUI/Dtos/RegisterDto/ResultUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class ResultUserDto
+    {
+       
+        public string FirstName { get; set; }     
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+    }
+}
