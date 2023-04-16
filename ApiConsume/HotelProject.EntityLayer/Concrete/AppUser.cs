3a60c9ceec71e0c4213123bc42411e59d843a4ab
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-17 00:12:57 +0300

    Create new entities.

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+    }
+}
