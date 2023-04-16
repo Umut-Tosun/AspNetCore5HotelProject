a7cf37757632c3599b197512bc6db03bf41b5f45
tosun <tosun@LAPTOP-ETHQG58O>
2023-04-17 00:09:36 +0300

    Add login dto.

---
+++ Client/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage ="Bu Alan Zorunludur")]
+       public string userName { get; set; }
+        [Required(ErrorMessage = "Bu Alan Zorunludur")]
+       public string password { get; set; }
+    }
+}
